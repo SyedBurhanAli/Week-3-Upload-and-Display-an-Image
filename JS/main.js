ff86da3ded3784b19af289f98816e4b272f62d34
Syed Burhan Ali Shah <iburhan2018@gmail.com>
2020-10-09 00:14:37 +0500

    Added "Make Gray Scale Functionality"

--- JS/main.js
+++ JS/main.js
@@ -18,9 +18,26 @@
         Create a SimpleImage from the chosen file. The SimpleImage library can be found at https://www.dukelearntoprogram.com/course1/common/js/image/SimpleImage.js. Remember you will need to use the <script src=’ ‘></script> tags in the HTML pane to tell your web page where to find the Simple Image library.
         Get the canvas element, and draw the image on the canvas. Note that you can define only one of the width or height of the canvas to avoid changing the image aspect ratio. */
 
+var image; //Global Variable so upload() and makeGray() both can use it.
+
+//But Try to Minimize the use of Global Variables. Relying on too many can make it hard to understand the code you're developing and how the functions interact with each other.
+
 function upload() {
   var imgcanvas = document.getElementById("can");
   var fileinput = document.getElementById("finput");
-  var image = new SimpleImage(fileinput);
+  // var image = new SimpleImage(fileinput);
+  image = new SimpleImage(fileinput);
   image.drawTo(imgcanvas); //Method inclusded in SimpleImage Libraray. We will call image.drawTo and use the canvas element as the parameter to indicate the SimpleImage should be drawn on a specific canvas of our choosing.
+}
+
+function makeGray() {
+  for (var pixel of image.values()) {
+    var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+
+    pixel.setRed(avg);
+    pixel.setGreen(avg);
+    pixel.setBlue(avg);
+  }
+  var imgcanvas = document.getElementById("can");
+  image.drawTo(imgcanvas);
 }
